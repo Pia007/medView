0617777df1a590c2fbc9ba05cfd08ad9cafa682e
Pia <piatorain@yahoo.com>
2022-10-25 10:55:08 -0700

    integrated react app, added ClientForward controller, updated app props

---
+++ src/main/java/dev/pia/mediconnect/controllers/ClientForwardController.java
@@ -0,0 +1,45 @@
+package dev.pia.mediconnect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientForwardController {
+
+    /* fowarding any request to the spring boot app to the react application \
+        using regex to parse the path coming in from the browser and looking at 
+        everything except for things that start with the dot (.) character.
+
+        For pages that you want to be handled by spring boot, you can add (.)
+
+    
+    */
+    
+    @GetMapping(value = "/**/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/";
+    }
+    
+}
+
+/*
+ * APPLICATION FAILED TO START
+ ***************************
+ * 
+ * Description:
+ * 
+ * Invalid mapping pattern detected: the regex pattern is the issue
+ * 
+ * No more pattern data allowed after {*...} or ** pattern element
+ * 
+ * Action:
+ * 
+ * Fix this pattern in your application or switch to the legacy parser
+ * implementation with
+ * 'spring.mvc.pathmatch.matching-strategy=ant_path_matcher'.
+ * 
+ * Fix: application.properties
+ * Cut, paste and update spring.mvc.pathmatch.matching-strategy=ant_path_matcher
+ * Restart the application
+ * 
+ */
